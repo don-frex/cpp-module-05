fb7f5a3bac0d68b5ec9a8d2a7f37c534d59bf701
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-01-02 15:36:32 +0100

    Add files via upload

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaber <asaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 18:06:29 by asaber            #+#    #+#             */
+/*   Updated: 2024/01/01 18:11:59 by asaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		std::string name;
+		int grade;
+	public:
+		
+		
+}
+
+#endif
